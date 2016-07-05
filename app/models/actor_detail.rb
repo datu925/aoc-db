675cb444d5cb1373568200dffd964ec0beb3b02a
Dan Turcza <datu925@gmail.com>
2016-07-05 13:18:28 +0800

    Add more entertaining default headshot

--- app/models/actor_detail.rb
+++ app/models/actor_detail.rb
@@ -1,9 +1,9 @@
 class ActorDetail < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :headshot, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :headshot, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/missing.jpg"
     validates_attachment_content_type :headshot, content_type: /\Aimage\/.*\Z/
 
-  has_attached_file :resume, default_url: "/images/:style/missing.png"
+  has_attached_file :resume, default_url: "/assets/missing.jpg"
   validates_attachment_content_type :resume, content_type: /\Aapplication\/pdf\Z/, message: "PDF only, please"
 
 
